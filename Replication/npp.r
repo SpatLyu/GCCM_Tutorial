10d5a5d3cc15ed61bd53a88d171375389e478015
SpatLyu <lyu.geosocial@gmail.com>
2025-10-23 21:31:08 +0800

    update scripts

--- Replication/npp.r
+++ Replication/npp.r
@@ -15,13 +15,13 @@ npp = terra::rast('./data/npp.tif')
 g1 = spEDM::gccm(npp, "pre", "npp", E = 3, k = 5, style = 0, stack = T,
                  libsizes = matrix(rep(seq(20,550,40),2), ncol = 2),
                  pred = as.matrix(expand.grid(seq(10,480,10),seq(10,550,10))),
-                 dist.metric = "L1", dist.average = F, window.ratio = 0.2, detrend = T)
+                 dist.metric = "L1", dist.average = F, win.ratio = 0.2, detrend = T)
 g1
 g1$xmap
 
 g2 = spEDM::gccm(npp, "tem", "npp", E = 4, k = 6, style = 0, stack = T,
                  libsizes = matrix(rep(seq(20,550,40),2), ncol = 2),
                  pred = as.matrix(expand.grid(seq(10,480,10),seq(10,550,10))),
-                 dist.metric = "L1", dist.average = F, window.ratio = 0.2, detrend = T)
+                 dist.metric = "L1", dist.average = F, win.ratio = 0.2, detrend = T)
 g2
 g2$xmap
---
+++ Replication/npp_vig.r
@@ -0,0 +1,20 @@
+library(spEDM)
+
+npp = terra::rast(system.file("case/npp.tif", package = "spEDM"))
+npp
+
+# Inspect NA values
+terra::global(npp,"isNA")
+terra::ncell(npp)
+nnamat = terra::as.matrix(npp[[1]], wide = TRUE)
+nnaindice = which(!is.na(nnamat), arr.ind = TRUE)
+dim(nnaindice)
+
+# Select 1500 non-NA pixels to predict:
+set.seed(2025)
+indices = sample(nrow(nnaindice), size = 1500, replace = FALSE)
+libindice = nnaindice[-indices,]
+predindice = nnaindice[indices,]
+
+s1 = simplex(npp, "pre", "pre", E = 2:10, k = 8, stack = TRUE, lib = nnaindice, pred = predindice)
+s2 = simplex(npp, "npp", "npp", E = 2:10, k = 8, stack = TRUE, lib = nnaindice, pred = predindice)
