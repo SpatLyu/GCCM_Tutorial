6efc0ac0c73722485517bc238b862a6b6ce2f666
SpatLyu <lyu.geosocial@gmail.com>
2025-10-24 02:03:30 +0800

    update

--- Replication/npp_vig.r
+++ Replication/npp_vig.r
@@ -16,7 +16,35 @@ indices = sample(nrow(nnaindice), size = 1500, replace = FALSE)
 libindice = nnaindice[-indices,]
 predindice = nnaindice[indices,]
 
-s1 = simplex(npp, "pre", "pre", E = 2:5, k = 3:8, style = 0, tau = 1, stack = TRUE, 
+startTime = Sys.time()
+npp_res = gccm(data = npp,
+               cause = "pre",
+               effect = "npp",
+               libsizes = matrix(rep(seq(20,400,40),2),ncol = 2),
+               E = 3,
+               k = 8,
+               style = 0,
+               tau = 1,
+               stack = TRUE,
+               lib = nnaindice,
+               pred = predindice,
+               dist.metric = "L1", 
+               dist.average = FALSE,
+               win.ratio = 0.2,
+               progressbar = FALSE)
+endTime = Sys.time()
+print(difftime(endTime,startTime, units ="mins"))
+npp_res
+npp_res$xmap
+readr::write_rds(npp_res,'./npp_res.rds')
+plot(npp_res)
+
+s1 = simplex(npp, "pre", "pre", E = 3:5, k = 4:8, style = 0, tau = 1, stack = TRUE, 
              lib = nnaindice, pred = predindice, dist.metric = "L1", dist.average = FALSE)
-s2 = simplex(npp, "npp", "npp", E = 2:10, k = 3:8, style = 0, tau = 1, stack = TRUE, 
+s1$xmap
+s1
+
+s2 = simplex(npp, "npp", "npp", E = 3:5, k = 4:8, style = 0, tau = 1, stack = TRUE, 
              lib = nnaindice, pred = predindice, dist.metric = "L1", dist.average = FALSE)
+s2$xmap
+s2
