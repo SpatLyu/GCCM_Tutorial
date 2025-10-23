ce1765a7f14722d601a6bf0f1e69aa727fc4e6a7
SpatLyu <lyu.geosocial@gmail.com>
2025-10-23 20:34:11 +0800

    update npp script

--- Replication/npp.r
+++ Replication/npp.r
@@ -9,16 +9,16 @@ names(npp) = c("pre","tem","npp")
 terra::trans(npp) |> 
   terra::plot()
 
-g1 = spEDM::gccm(npp, "pre", "npp", E = 4, k = 6, style = 0, stack = 1,
-                 libsize = matrix(rep(300,2), ncol = 2),
+g1 = spEDM::gccm(npp, "pre", "npp", E = 3, k = 5, style = 0, stack = T,
+                 libsizes = matrix(rep(seq(20,550,40),2), ncol = 2),
                  pred = as.matrix(expand.grid(seq(10,480,10),seq(10,550,10))),
-                 dist.metric = "L1", dist.average = F, detrend = T)
+                 dist.metric = "L1", dist.average = F, window.ratio = 0.2, detrend = T)
 g1
 g1$xmap
 
-g2 = spEDM::gccm(npp, "tem", "npp", E = 4, k = 6, style = 0, stack = 1,
-                 libsize = matrix(rep(300,2), ncol = 2),
+g2 = spEDM::gccm(npp, "tem", "npp", E = 4, k = 6, style = 0, stack = T,
+                 libsizes = matrix(rep(seq(20,550,40),2), ncol = 2),
                  pred = as.matrix(expand.grid(seq(10,480,10),seq(10,550,10))),
-                 dist.metric = "L1", dist.average = F, detrend = T)
+                 dist.metric = "L1", dist.average = F, window.ratio = 0.2, detrend = T)
 g2
 g2$xmap
